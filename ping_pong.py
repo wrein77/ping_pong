ef5bfcd83fd00e8efcfa867589d94443caabec9c
wrein77 <sidrisov086@gmail.com>
2026-03-10 15:21:19 +0500

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -12,14 +12,21 @@ class gamesprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(gamesprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 620:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 15:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 390:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 15:
+            self.rect.y-= self.speed
+        if keys[K_s] and self.rect.y < 390:
+            self.rect.y += self.speed
+racket1 = Player('racket.png', 5, 50, 3, 30, 120)
+racket2 = Player('racket.png', 660, 50, 3, 30, 120)
 window = display.set_mode((700, 500))
-window.fill((0, 170, 255))
 fps = 60
 clock = time.Clock()
 game = True
@@ -27,5 +34,10 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    window.fill((0, 170, 255))
+    racket1.update_l()
+    racket2.update_r()
+    racket1.reset()
+    racket2.reset()
     display.update()
     clock.tick(fps)
